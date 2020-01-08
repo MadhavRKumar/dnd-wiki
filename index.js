68aec8a57e50af1a381d11ae3d4be628da58bd99
Madhav Kumar <madhavrkumar@gmail.com>
2020-01-08 12:30:06 -0500

    Create Edit Article function

--- index.js
+++ index.js
@@ -15,14 +15,58 @@ app.use(cors());
 
 const getArticle = (req, res) => {
     
-    pool.query( `SELECT text FROM text WHERE id = (SELECT rev_text_id FROM revisions WHERE id = (SELECT page_latest FROM pages WHERE lower(page_title) = lower('${req.params.pageTitle}')));`,
+    pool.query( 'SELECT text FROM text WHERE id = (SELECT rev_text_id FROM revisions WHERE id = (SELECT page_latest FROM pages WHERE lower(page_title) = lower($1)));', [req.params.pageTitle],
         
         (err, result) => {
             if(err) {
                 return console.error('Error executing query', err.stack)
             }
-            res.status(200).json(result.rows)  
-        })
+            res.status(200).json(result.rows[0])  
+         })
+}
+
+const createArticle = (req, res) => {
+    const { pageTitle, text } = req.body;
+
+    
+}
+
+const editArticle = (req, res) => {
+    const { pageTitle, text } = req.body;
+
+    (async () => {
+        const client = await pool.connect();
+
+        try {
+            await client.query('BEGIN');
+
+            const pageRes = await client.query('SELECT id FROM pages WHERE page_title = $1', [pageTitle]);
+            const pageID = pageRes.rows[0].id;
+            
+            const textRes = await client.query('INSERT INTO text (text) VALUES ($1) RETURNING id', [text]);
+            const textID = textRes.rows[0].id;
+
+            const insertRevValues = [pageID, textID];
+            const revRes = await client.query('INSERT INTO revision (rev_page, rev_text_id) VALUES ($1, $2) RETURNING id', insertRevValues);
+            const revID = revRes.rows[0].id;
+            const updateRevValues = [revID, pageID];
+
+            await client.query('UPDATE pages SET page_latest = $1 WHERE id = $2', [updateRevValues]);
+            await client.query('COMMIT');
+
+        } catch (err) {
+            await client.query('ROLLBACK');
+            throw err;
+        }
+        finally {
+            client.release();
+            res.status(201).send(`Article '${pageTitle}' successfully edited`);
+        }
+
+
+    })().catch(err => console.error('Error executing query', err.stack))
+
+    
 }
 
 
