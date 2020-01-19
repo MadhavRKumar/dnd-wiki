fdc0ac17a745e03518c6b692a1820a453f4e9435
Madhav Kumar <madhavrkumar@gmail.com>
2020-01-19 13:00:00 -0500

    Test connection between front and back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Article from './components/Article';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: "test2"}
+  }
+
+  callAPI = async (pageTitle) => {
+    const response = await fetch(`/${pageTitle}`);
+    const body = await response.json();
+    if(response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  componentDidMount() {
+    this.callAPI(this.state.title).then(res => this.setState({text: res.text})).catch(err => console.error(err));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Article title={this.state.title} text={this.state.text} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
