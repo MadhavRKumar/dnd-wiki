3b14287fcb8f2098cf89efa76db6481df9a5b148
Madhav Kumar <madhavrkumar@gmail.com>
2020-01-21 00:58:04 -0500

    Implement use of React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import Article from './components/Article';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {title: "test2"}
-  }
-
-  callAPI = async (pageTitle) => {
-    const response = await fetch(`/${pageTitle}`);
-    const body = await response.json();
-    if(response.status !== 200) throw Error(body.message);
-
-    return body;
-  }
-
-  componentDidMount() {
-    this.callAPI(this.state.title).then(res => this.setState({text: res.text})).catch(err => console.error(err));
-  }
 
 
   render() {
     return (
-      <div className="App">
-        <Article title={this.state.title} text={this.state.text} />
-      </div>
+      <Router>
+        <Switch>
+          <Route path='/article/:pageTitle'>
+            <Article />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 
