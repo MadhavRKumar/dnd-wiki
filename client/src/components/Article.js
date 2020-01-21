85f48fa2769f844a01deded6dad9319583cb2d68
Madhav Kumar <madhavrkumar@gmail.com>
2020-01-21 00:58:51 -0500

    Change Article to use React Hooks

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-function Article(props) {
+function parseTitle(pageTitle) {
+    return pageTitle.replace('_', ' ');
+}
+function Article() {
+    let { pageTitle } = useParams();
+    let parsedTitle = parseTitle(pageTitle);
+    let [text, setText] = useState(null);
+    let callAPI = async (pageTitle) => {
+        const response = await fetch(`/${pageTitle}`);
+        const body = await response.json();
+        if (response.status !== 200) 
+        {
+             throw Error(body.message);
+        }
+        return body;
+    };
+
+    useEffect(() => {
+        callAPI(parsedTitle).then(res => setText(res.text)).catch(err => console.error(err));
+    });
+    
+
+
     return (
-    <div>
-        <h1 className="title">{props.title}</h1>
-        <p className="text">{props.text}</p>    
-    </div>
-    )
+        <div>
+            <h1 className="title">{parsedTitle}</h1>
+            <p className="text">{text}</p>
+        </div>
+    );
 }
 
 export default Article;
