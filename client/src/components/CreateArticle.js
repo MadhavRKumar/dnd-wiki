b1c3adbb94b92a6595c3d7c7c82d7d337c6fe810
MadhavRKumar <madhavrkumar@gmail.com>
2020-02-09 14:13:09 -0500

    Styled Title input
    Also handled input verification

--- client/src/components/CreateArticle.js
+++ client/src/components/CreateArticle.js
@@ -9,33 +9,39 @@ export default function CreateArticle() {
     let history = useHistory();
 
     function handleSubmit() {
-        var myHeaders = new Headers();
+        let myHeaders = new Headers();
         myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
-        let title = titleInput.current.value;
-        var urlencoded = new URLSearchParams();
-        urlencoded.append('text', input.current.value);
-
-        var requestOptions = {
-            method: 'PUT',
-            headers: myHeaders,
-            body: urlencoded,
-            redirect: 'follow'
-        };
-
-        fetch('/' + title, requestOptions)
-            .then(response => response.text())
-            .then(result => {
-                alert(result);
-                history.push('/article/' + title);
-            })
-            .catch(error => console.log('error', error));
+        const title = titleInput.current.value;
+        const text = input.current.value;
+        let urlencoded = new URLSearchParams();
+        urlencoded.append('text',text);
+        if(title && text) {
+            let requestOptions = {
+                method: 'PUT',
+                headers: myHeaders,
+                body: urlencoded,
+                redirect: 'follow'
+            };
+    
+            fetch('/' + title, requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    alert(result);
+                    history.push('/article/' + title);
+                })
+                .catch(error => console.log('error', error));
+        }
+        else {
+            alert("Article Body and Title required");
+        }
+   
     }
 
 
     return (
         <>
         <Article>
-            <input ref={titleInput} type="text" className="title-input"/>
+            <input ref={titleInput} type="text" name="title" placeholder="Title" className="title-input"/>
             <Editor input={input} text={{}}/>
         </Article>
         <button className="edit-button" onClick={handleSubmit}>Submit</button>
