763d11a8421c779241d55174e167da958f0866b0
MadhavRKumar <madhavrkumar@gmail.com>
2020-02-24 23:04:55 -0500

    Add 'Create Article' Link

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Search from './Search';
 import Loader from './Loader';
 import logo from './logo.png';
+import { Link } from 'react-router-dom';
 import useLoading from '../hooks/useLoading';
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
             <div className="flex-container-centered">
                 <img src={logo} alt="Green DnD 5e with the word WIKI underneath" className="logo" />
                 <Search />
+                <Link className="create-link" to="/create" >Create Article</Link>
             </div>);
     }
 
