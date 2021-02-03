333a86db0b1094019e6edf8fa53c0b72b1971af4
MadhavRKumar <MadhavRKumar@gmail.com>
2021-02-02 23:08:47 -0500

    Force HTTPS

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
+
 const getArticle = (req, res) => {
 
     pool.query('SELECT text FROM text WHERE id = (SELECT rev_text_id FROM revisions WHERE id = (SELECT page_latest FROM pages WHERE lower(page_title) = lower($1)));', [req.params.pageTitle],
